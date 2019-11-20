eb42d40aa3fb338b7cf3dc93179b52d85da512d0
pietralbi <17704110+pietralbi@users.noreply.github.com>
2019-11-20 23:17:46 +0900

    adding woollen clothes, still need to finish

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+foldlink=UnrealWorld.lnk
+
+cp diy_*txt $foldlink
+cp biy_*.txt $foldlink
+cp menudef_sufficiency.txt  $foldlink
+cp cookery_glossary.txt $foldlink
+cp flora_* $foldlink
+cp GAME.NFO $foldlink
+cp -r truegfx $foldlink
+cp -r truetile $foldlink
